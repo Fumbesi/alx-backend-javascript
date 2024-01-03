72fbaef8e844c333144b80f5273bcc2418091f4d
Khwezi Ndlovu <khwezindlovu25@gmail.com>
2024-01-03 06:55:27 -0800

    2

---
+++ 0x01-ES6_promise/2-then.js
@@ -0,0 +1,14 @@
+// 2-then.js
+function handleResponseFromAPI(promise) {
+  return promise
+    .then(() => {
+      console.log('Got a response from the API');
+      return { status: 200, body: 'success' };
+    })
+    .catch(() => {
+      console.log('Got a response from the API');
+      return new Error();
+    });
+}
+
+export default handleResponseFromAPI;
