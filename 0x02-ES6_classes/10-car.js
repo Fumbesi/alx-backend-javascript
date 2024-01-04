db7a50793fd77af1095e9d09271c1cdf766aea30
Khwezi Ndlovu <khwezindlovu25@gmail.com>
2024-01-04 06:38:19 -0800

    10

--- 0x02-ES6_classes/10-car.js
+++ 0x02-ES6_classes/10-car.js
@@ -1,18 +1,31 @@
-const cloneSymbol = Symbol('clone');
+const cloneCarSymbol = Symbol('cloneCarSymbol');
 
 export default class Car {
   constructor(brand, motor, color) {
     this._brand = brand;
     this._motor = motor;
     this._color = color;
+  }
 
-    this[cloneSymbol] = () => {
-      const clonedCar = new Car(this._brand, this._motor, this._color);
-      return clonedCar;
-    };
+  get brand() {
+    return this._brand;
+  }
+
+  get motor() {
+    return this._motor;
+  }
+
+  get color() {
+    return this._color;
+  }
+
+  [cloneCarSymbol]() {
+    const { constructor, ...props } = this;
+    const clonedCar = new this.constructor(...Object.values(props));
+    return clonedCar;
   }
 
   cloneCar() {
-    return this[cloneSymbol]();
+    return this[cloneCarSymbol]();
   }
 }
