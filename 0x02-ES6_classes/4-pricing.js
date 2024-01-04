08019c6b624450289f088b3b07332c493cf335cd
Khwezi Ndlovu <khwezindlovu25@gmail.com>
2024-01-04 06:01:23 -0800

    4

---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,34 @@
+export default class Pricing {
+  constructor(amount, currency) {
+    this._amount = amount;
+    this._currency = currency;
+  }
+
+  // Getter and setter for 'amount'
+  get amount() {
+    return this._amount;
+  }
+
+  set amount(newAmount) {
+    this._amount = newAmount;
+  }
+
+  // Getter and setter for 'currency'
+  get currency() {
+    return this._currency;
+  }
+
+  set currency(newCurrency) {
+    this._currency = newCurrency;
+  }
+
+  // Method to display full price details
+  displayFullPrice() {
+    return `${this._amount} ${this._currency.name} (${this._currency.code})`;
+  }
+
+  // Static method to convert price based on conversion rate
+  static convertPrice(amount, conversionRate) {
+    return amount * conversionRate;
+  }
+}
