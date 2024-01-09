38dbe1771ec3e1c0e6682ed0a26b2b5cd0096d2d
Khwezi Ndlovu <khwezindlovu25@gmail.com>
2024-01-09 03:36:02 -0800

    2

---
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -0,0 +1,7 @@
+// 2-get_students_by_loc.js
+
+function getStudentsByLocation(studentsList, city) {
+  return studentsList.filter((student) => student.location === city);
+}
+
+module.exports = getStudentsByLocation;
