5f41eb66cd5cd09544c54f8527c6f90a5de429d1
Khwezi Ndlovu <khwezindlovu25@gmail.com>
2024-01-10 04:23:23 -0800

    mainadd2

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -50,3 +50,40 @@ function createEmployee(salary: number | string): Director | Teacher {
 console.log(createEmployee(200)); // Output: Teacher
 console.log(createEmployee(1000)); // Output: Director
 console.log(createEmployee('$500')); // Output: Director
+
+
+interface DirectorInterface {
+  workDirectorTasks(): string;
+}
+
+interface TeacherInterface {
+  workTeacherTasks(): string;
+}
+
+class Director implements DirectorInterface {
+  workDirectorTasks(): string {
+    return 'Getting to director tasks';
+  }
+}
+
+class Teacher implements TeacherInterface {
+  workTeacherTasks(): string {
+    return 'Getting to work';
+  }
+}
+
+function isDirector(employee: Director | Teacher): employee is Director {
+  return (employee as Director).workDirectorTasks !== undefined;
+}
+
+function executeWork(employee: Director | Teacher): string {
+  if (isDirector(employee)) {
+    return employee.workDirectorTasks();
+  } else {
+    return (employee as Teacher).workTeacherTasks();
+  }
+}
+
+// Example usage
+console.log(executeWork(createEmployee(200))); // Output: Getting to work
+console.log(executeWork(createEmployee(1000))); // Output: Getting to director tasks
