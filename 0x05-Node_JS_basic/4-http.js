56ed3a1d51712757342e0c3c8e5434a1af72bcb7
Fumbesi <124091057+Fumbesi@users.noreply.github.com>
2024-02-28 12:51:34 +0200

    Update 4-http.js

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,18 +1,7 @@
-// 4-http.js
-
 const http = require('http');
 
 const app = http.createServer((req, res) => {
-  // Set the response header with content type
-  res.setHeader('Content-Type', 'text/plain');
-
-  // Send the response body
-  res.end('Hello Holberton School!\n');
+  res.end('Hello Holberton School!');
 });
 
-// Listen on port 1245
 app.listen(1245);
-
-// Export the app for external use
-module.exports = app;
-
