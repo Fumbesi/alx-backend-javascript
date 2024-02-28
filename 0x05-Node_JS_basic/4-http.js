b77f9af444592254f842862e43963958398e58c6
Fumbesi <124091057+Fumbesi@users.noreply.github.com>
2024-02-28 13:29:20 +0200

    Update 4-http.js

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,7 +1,13 @@
 const http = require('http');
 
+const port = 1245;
+const host = '127.0.0.1';
+
 const app = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
   res.end('Hello Holberton School!');
 });
 
-app.listen(1245);
+app.listen(port, host, () => {});
+module.exports = app;
