06b8e379f786e868e1fbb60c03dbb8673349e479
Fumbesi <124091057+Fumbesi@users.noreply.github.com>
2024-02-28 13:31:48 +0200

    Update 6-http_express.js

--- 0x05-Node_JS_basic/6-http_express.js
+++ 0x05-Node_JS_basic/6-http_express.js
@@ -1,19 +1,12 @@
-// 6-http_express.js
-
 const express = require('express');
 
 const app = express();
+const port = 1245;
 
-// Set up a route for the root endpoint
 app.get('/', (req, res) => {
-  res.send('Hello Holberton School!\n');
+  res.send('Hello Holberton School!');
 });
 
-// Listen on port 1245
-const server = app.listen(1245, () => {
-  console.log('Server is listening on port 1245');
-});
+app.listen(port, () => {});
 
-// Export the app for external use
 module.exports = app;
-
