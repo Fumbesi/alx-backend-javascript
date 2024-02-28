07f0c9e88d774b8495424dc3f962a82486d22729
Fumbesi <124091057+Fumbesi@users.noreply.github.com>
2024-02-28 13:30:27 +0200

    Update 5-http.js

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -1,45 +1,70 @@
-// 5-http.js
 
 const http = require('http');
-const countStudents = require('./3-read_file_async');
+const { readFile } = require('fs');
 
+const port = 1245;
+const host = '127.0.0.1';
+
+function countStudents (path) {
+  const students = {};
+  return new Promise((resolve, reject) => {
+    readFile(path, (err, data) => {
+      const results = [];
+      if (err) {
+        reject(Error('Cannot load the database'));
+      } else {
+        const content = data.toString().trim();
+        const lines = content.split('\n');
+        let length = 0;
+        for (let i = 0; i < lines.length; i++) {
+          const line = lines[i].toString();
+          if (!line) continue;
+          const values = line.split(',');
+          const field = values[3].trim();
+          const firstname = values[0].trim();
+
+          if (field === 'field') continue;
+          if (students[field] !== undefined) {
+            students[field].push(firstname);
+          } else {
+            students[field] = [firstname];
+          }
+          length++;
+        }
+        results.push(`Number of students: ${length}`);
+        for (const field in students) {
+          const names = students[field];
+          results.push(
+            `Number of students in ${field}: ${
+              names.length
+            }. List: ${names.join(', ')}`,
+          );
+        }
+        resolve(results.join('\n'));
+      }
+    });
+  });
+}
 const app = http.createServer((req, res) => {
-  // Set the response header with content type
+  res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
+  if (req.url === '/') {
+    res.end('Hello Holberton School!');
+  }
+  if (req.url === '/students') {
+    res.write('This is the list of our students\n');
+    const path = process.argv[2].toString();
 
-  // Parse the URL
-  const url = new URL(req.url, `http://${req.headers.host}`);
-
-  // Handle different routes
-  if (url.pathname === '/') {
-    // Handle root route
-    res.end('Hello Holberton School!\n');
-  } else if (url.pathname === '/students') {
-    // Handle /students route
-
-    // Get the database file name from command line arguments
-    const [, , databaseFileName] = process.argv;
-
-    // Call the countStudents function with the database file name
-    countStudents(databaseFileName)
-      .then(() => {
-        // This is the list of our students
-        res.end('This is the list of our students\n');
+    countStudents(path)
+      .then(data => {
+        res.end(data.toString().trim());
       })
-      .catch((error) => {
-        // Log the error if there's an issue reading the database
-        console.error(error.message);
-        res.end('Internal Server Error\n');
+      .catch(err => {
+        res.statusCode = 500;
+        res.end('Cannot load the database');
       });
-  } else {
-    // Handle other routes
-    res.end('Not Found\n');
   }
 });
 
-// Listen on port 1245
-app.listen(1245);
-
-// Export the app for external use
+app.listen(port, host, () => {});
 module.exports = app;
-
