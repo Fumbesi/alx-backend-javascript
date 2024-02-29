206cae595a9acb5b1f75f41317ca82bcdf53f60d
Fumbesi <124091057+Fumbesi@users.noreply.github.com>
2024-02-29 21:02:18 +0200

    Update utils.js

--- 0x05-Node_JS_basic/full_server/utils.js
+++ 0x05-Node_JS_basic/full_server/utils.js
@@ -1,35 +1,43 @@
-// const { readFile } = require("fs");
-import { readFile } from 'fs';
+import fs from 'fs';
 
-function readDatabase (path) {
-  const students = {};
-  return new Promise((resolve, reject) => {
-    readFile(path, (err, data) => {
-      if (err) {
-        reject(Error('Cannot load the database'));
-      } else {
-        const content = data.toString();
-        const lines = content.split('\n');
-        for (let i = 0; i < lines.length; i++) {
-          const line = lines[i];
-          if (!line) continue;
-          const values = line.split(',');
-          const field = values[3].trim();
-          const firstname = values[0].trim();
-
-          if (field === 'field') continue;
-          if (students[field] !== undefined) {
-            students[field].push(firstname);
-          } else {
-            students[field] = [firstname];
+const readDatabase = (path) => new Promise((res, rej) => {
+  fs.readFile(path, 'utf8', (err, data) => {
+    if (err) {
+      rej(err);
+    } else {
+      let minus = 1;
+      let result = '';
+      // eslint-disable-next-line  no-param-reassign
+      data = data.split('\n');
+      const fieldlist = {};
+      for (let i = 1; i < data.length; i += 1) {
+        const firstName = data[i].split(',')[0];
+        const field = data[i].split(',')[3];
+        if (!Object.prototype.hasOwnProperty.call(fieldlist, field)) {
+          if (field === undefined) {
+            minus += 1;
+            // eslint-disable-next-line no-continue
+            continue;
           }
+          fieldlist[field] = [];
         }
-
-        resolve(students);
+        fieldlist[field].push(firstName);
       }
-    });
+      result += `Number of students: ${data.length - minus}\n`;
+      let i = 0;
+      for (const key in fieldlist) {
+        if (Object.prototype.hasOwnProperty.call(fieldlist, key)) {
+          result += `Number of students in ${key}: ${fieldlist[key].length}. List: ${fieldlist[key].join(', ')}`;
+          // eslint-disable-next-line no-continue
+          if (i === 1) continue;
+          result += '\n';
+          i += 1;
+        }
+      }
+      res({ result, fieldlist });
+    }
   });
-}
+});
 
-// module.exports = readDatabase;
 export default readDatabase;
+module.exports = readDatabase;
