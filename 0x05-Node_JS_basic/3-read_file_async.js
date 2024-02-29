e009ca2dd231547a2087b528c51d42bab7f3ca1f
Fumbesi <124091057+Fumbesi@users.noreply.github.com>
2024-02-29 20:53:59 +0200

    Update 3-read_file_async.js

--- 0x05-Node_JS_basic/3-read_file_async.js
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -1,54 +1,36 @@
 const fs = require('fs');
 
-function countStudents(path) {
-  return new Promise((resolve, reject) => {
-    fs.readFile(path, 'utf8', (error, data) => {
-      if (error) {
-        reject(new Error('Cannot load the database'));
-        return;
+module.exports = function countStudents(path) {
+  return new Promise((res, rej) => {
+    fs.readFile(path, 'utf8', (err, data) => {
+      if (err) {
+        rej(new Error('Cannot load the database'));
+      } else {
+        let minus = 1;
+        // eslint-disable-next-line no-param-reassign
+        data = data.split('\n');
+        const fieldlist = {};
+        for (let i = 1; i < data.length; i += 1) {
+          const firstName = data[i].split(',')[0];
+          const field = data[i].split(',')[3];
+          if (!Object.prototype.hasOwnProperty.call(fieldlist, field)) {
+            if (field === undefined) {
+              minus += 1;
+              // eslint-disable-next-line no-continue
+              continue;
+            }
+            fieldlist[field] = [];
+          }
+          fieldlist[field].push(firstName);
+        }
+        console.log(`Number of students: ${data.length - minus}`);
+        for (const key in fieldlist) {
+          if (Object.prototype.hasOwnProperty.call(fieldlist, key)) {
+            console.log(`Number of students in ${key}: ${fieldlist[key].length}. List: ${fieldlist[key].join(', ')}`);
+          }
+        }
+        res();
       }
-
-      // Split the data into rows
-      const rows = data.trim().split('\n');
-
-      // Initialize counters
-      let totalStudents = 0;
-      let csStudents = 0;
-      let sweStudents = 0;
-      let csList = [];
-      let sweList = [];
-
-      // Iterate through each row
-      rows.forEach(row => {
-        // Skip empty lines
-        if (row.trim() === '') {
-          return;
-        }
-
-        // Parse CSV values
-        const [firstname, lastname, age, field] = row.split(',');
-
-        // Check if the field is CS or SWE
-        if (field === 'CS') {
-          csStudents++;
-          csList.push(firstname);
-        } else if (field === 'SWE') {
-          sweStudents++;
-          sweList.push(firstname);
-        }
-
-        totalStudents++;
-      });
-
-      // Log the results
-      console.log(`Number of students: ${totalStudents}`);
-      console.log(`Number of students in CS: ${csStudents}. List: ${csList.join(', ')}`);
-      console.log(`Number of students in SWE: ${sweStudents}. List: ${sweList.join(', ')}`);
-
-      resolve();
     });
   });
-}
-
-module.exports = countStudents;
-
+};
