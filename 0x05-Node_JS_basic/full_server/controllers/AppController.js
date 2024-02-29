e6ed4a6e857561d955c957600c7df927421ff3a2
Fumbesi <124091057+Fumbesi@users.noreply.github.com>
2024-02-29 20:59:34 +0200

    Update AppController.js

--- 0x05-Node_JS_basic/full_server/controllers/AppController.js
+++ 0x05-Node_JS_basic/full_server/controllers/AppController.js
@@ -1,8 +1,9 @@
 class AppController {
-  static getHomepage (request, response) {
-    // response.statusCode = 200;
-    // response.end("Hello Holberton School!");
-    response.status(200).send('Hello Holberton School!');
+  static getHomepage(request, response) {
+    response.statusCode = 200;
+    response.send('Hello Holberton School!');
   }
 }
+
 export default AppController;
+module.exports = AppController;
